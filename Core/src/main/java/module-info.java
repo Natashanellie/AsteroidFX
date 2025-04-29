877099930bfc07d8fadfb1756171674397f28a72
Natashanellie <143799247+Natashanellie@users.noreply.github.com>
2025-04-29 10:20:11 +0200

    firdt commit

---
+++ Core/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module Core {
+    requires Common;
+    requires CommonBullet;
+    requires javafx.graphics;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    exports dk.sdu.cbse;
+    opens dk.sdu.cbse to javafx.graphics,spring.core;
+    uses dk.sdu.cbse.common.services.IGamePluginService;
+    uses dk.sdu.cbse.common.services.IEntityProcessingService;
+    uses dk.sdu.cbse.common.services.IPostEntityProcessingService;
+}
